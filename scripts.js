c9cce6cfb895fa0e70fc66a9a6a74546f665c282
chris-newton <newton.chris685@gmail.com>
2024-09-14 03:23:31 -0400

    added svg icon and link for hero (home) section, added button animations

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 // accepts a selector for a section on the page and smotth scrolls to it
+function scrollToHeroSection() {
+    document.querySelector('.hero-section').scrollIntoView({ behavior: "smooth" });
+}
+
 function scrollToAboutSection() {
     document.querySelector('.about-section').scrollIntoView({ behavior: "smooth" });
 }
-
 
 function scrollToProjectsSection() {
     document.querySelector('.projects-section').scrollIntoView({ behavior: "smooth" });
